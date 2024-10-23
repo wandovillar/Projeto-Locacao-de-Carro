7c363c55b676058a355da7dd68e8ea6951db26b9
Wando Villar <wandovilar@outlook.com>
2024-10-23 11:47:08 -0300

    Adicionando arquivo de excercios para pratica no sql

---
+++ EXERCICIOS-PRATICOS-CLIQUE-AQUI.sql
@@ -0,0 +1,128 @@
+--  Aqui estão as perguntas de negócio reformuladas para usuários que desejam praticar exercícios em SQL:
+
+-- TENHA UM BOM DIVERTIMENTO :)
+
+
+-- 1) Clientes e Planos Atuais
+
+-- Exiba todos os clientes junto com o nome do plano que eles estão utilizando atualmente. Inclua informações como o nome e sobrenome do cliente e o nome do plano.
+
+
+
+
+
+
+
+
+
+-- 2) Clientes com Mais de Duas Contratações
+-- Liste os clientes que fizeram mais de duas contratações de locação. Exiba o nome completo e a quantidade de vezes que eles contrataram um serviço da LocaCar.
+
+
+
+
+
+
+
+
+
+-- 3) Valor Total de Locação por Cliente
+
+-- Calcule o valor total gasto por cada cliente em suas locações. A consulta deve exibir o nome completo do cliente e o valor total pago.
+
+
+
+
+
+
+
+
+-- 4) Planos Mais Contratados
+-- Exiba o número de contratações para cada plano. Liste o nome do plano e a quantidade de contratações.
+
+
+
+
+
+
+
+
+
+
+
+-- 5) Média de Valor Gasto por Plano
+-- Calcule a média de valor gasto por clientes em cada plano, considerando a soma do valorCaucao e valorMensalidade de cada contrato.
+
+
+
+
+
+
+
+
+
+-- 6) Clientes com Plano para Negativados
+-- Liste todos os clientes que estão utilizando o plano destinado a clientes negativados. Inclua o nome completo, status do pagamento e valor da última contratação.
+
+
+
+
+
+
+
+
+-- 7) Locações Inativas
+-- Exiba todas as locações que estão com o status de pagamento "Inativo". A consulta deve incluir o nome do cliente, o nome do plano e as datas de início e fim da locação.
+
+
+
+
+
+
+
+
+-- 8) Análise de Pagamentos Inativos
+-- Liste todas as locações onde o status de pagamento é "Inativo". Exiba o nome do cliente, o valor total da locação e o método de pagamento.
+
+
+
+
+
+
+
+
+
+
+
+
+-- 9) Mês com Maior Receita de Locação
+-- Calcule o mês que gerou a maior receita de locação para a empresa. A receita deve ser a soma das colunas valorCaucao e valorMensalidade de todas as locações em um determinado mês.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 10) Locações por Origem de Marca
+-- Exiba a quantidade de locações agrupadas pela origem dos carros (nacional ou importado).
+
+
+
+
+
+
+
+
+
+ -- Esses exercícios são excelentes para praticar habilidades em SQL e modelagem de consultas! 
+ -- BOAS PRÁTICAS !
+ -- ATT. Wando Vilar
